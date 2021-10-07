7ab3d48b6017c572234cbd12a6b311a7b9b4b5e5
sourovahmad <mdsourovahmad19@gmail.com>
2021-10-08 00:12:19 +0600

    array map

--- es6/map.js
+++ es6/map.js
@@ -8,10 +8,27 @@
 
 
 // this is a function
-const plus = (x, y = 0) => x + 2;
+const plus = (x) => x + 2;
 
 //array.map return first permater as element, second one index and third one as full array
 
-console.log(array.map(function(el){ plus(el)}));
+// console.log(array.map(el => plus(el)));
+
+
+// filter return a array with condition , using x as element;
+
+const filter = array.filter(x =>  x > 5);
+
+// console.log(filter);
+
+
+// find data will return the first value
+
+const find = array.find(x => x == 2)
+console.log(find);
+
+
+
+
 
 
