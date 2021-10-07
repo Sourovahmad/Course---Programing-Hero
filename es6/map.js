b8d2b3206d083f505e742e218a431392d8b868dd
sourovahmad <mdsourovahmad19@gmail.com>
2021-10-07 23:06:01 +0600

    javascitp map

---
+++ es6/map.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+  const array = [1, 2, 3, 4, 5];
+
+
+// this is a function
+const plus = (x, y = 0) => x + 2;
+
+//array.map return first permater as element, second one index and third one as full array
+
+console.log(array.map(function(el){ plus(el)}));
+
+
